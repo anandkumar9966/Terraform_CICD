fe34773c69f24a4f97c1ad3c79e0d077af2b52e3
anandkumar9966 <61936360+anandkumar9966@users.noreply.github.com>
2025-03-20 12:35:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "test" {
+resource "aws_instance" "prd" {
     ami = "ami-00db8dadb36c9815e"
     instance_type = "t2.micro"
     tags = {
